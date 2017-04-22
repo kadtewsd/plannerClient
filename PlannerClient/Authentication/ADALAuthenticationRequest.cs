69bdbba8268991bfa1e5a8143d4b1eab098e5970
kadtewsd <kasakaid@outlook.com>
2017-04-22 15:55:41 +0900

    不要な情報を削除

--- PlannerClient/Authentication/ADALAuthenticationRequest.cs
+++ PlannerClient/Authentication/ADALAuthenticationRequest.cs
@@ -19,14 +19,6 @@ namespace PlannerClient.Authentication
 
         public async Task<SignInModel> DoAuth()
         {
-            //string clientId = "cd898122-b3e3-4df7-af28-d2d24d6b3db5";
-            //const string clientId = "85e5ebc0-b31f-4cd7-b8a4-fc55602ecfcd"; //Web App for kasakaid。マルチテナントアプリは、Web Apps にしないといけない。マルチテナントアプリでないと同意は現れない。
-            //string clientSecret = "lF98J/T3ZKbhwTR/l9ylfzKjw7PqTvWHxQZ6/f1ZtAw=";
-            //string clientId = "184e570f-cb22-4987-bad4-2a6d559bcd77";// Native アプリ。Native アプリは同意がない。ただし、そのテナントでないと使えない。
-            //UserAssertion user = new UserAssertion();
-            //ClientAssertionCertificate clientAssertion = new ClientAssertionCertificate(;
-            //ClientCredential cred = new ClientCredential(clientId, clientSecret);
-
             AuthenticationContext ac = new AuthenticationContext(ConfigurationManager.AppSettings["LoginUri"] + "/common");
             //AuthenticationResult ar = await ac.AcquireTokenAsync(ResourceUri, cred);
             UserIdentifier identity = new UserIdentifier(form.UserName, UserIdentifierType.RequiredDisplayableId);
